8ccc60a7541301c0f13f2855009fb3bd82ad7492
Luka967 <luka.djokovic2@gmail.com>
2016-05-10 21:39:39 +0200

    Some info

--- src/Client/Packet.cs
+++ src/Client/Packet.cs
@@ -5,14 +5,17 @@ using System.Text;
 
 namespace Client
 {
+    // Class which is used to reference BitConverter. I like this name more :3
     public static class Packet
     {
+        // Size: 1
         public static ushort ReadUint8(byte[] array, ref int offset)
         {
             offset++;
             return array[offset - 1];
         }
 
+        // Size: 2
         public static ushort ReadUint16(byte[] array, ref int offset)
         {
             byte[] a = new byte[2];
@@ -24,6 +27,7 @@ namespace Client
             return BitConverter.ToUInt16(a, 0);
         }
 
+        // Size: 4
         public static uint ReadUint32(byte[] array, ref int offset)
         {
             byte[] a = new byte[4];
@@ -35,6 +39,7 @@ namespace Client
             return BitConverter.ToUInt32(a, 0);
         }
 
+        // Size: 8
         public static ulong ReadUint64(byte[] array, ref int offset)
         {
             byte[] a = new byte[8];
@@ -45,6 +50,8 @@ namespace Client
             }
             return BitConverter.ToUInt64(a, 0);
         }
+
+        // Size: 2
         public static short Readint16(byte[] array, ref int offset)
         {
             byte[] a = new byte[2];
@@ -56,6 +63,7 @@ namespace Client
             return BitConverter.ToInt16(a, 0);
         }
 
+        // Size: 4
         public static int Readint32(byte[] array, ref int offset)
         {
             byte[] a = new byte[4];
@@ -67,6 +75,7 @@ namespace Client
             return BitConverter.ToInt32(a, 0);
         }
 
+        // Size: 8
         public static long Readint64(byte[] array, ref int offset)
         {
             byte[] a = new byte[8];
@@ -78,6 +87,7 @@ namespace Client
             return BitConverter.ToInt64(a, 0);
         }
 
+        // Size: 4
         public static float ReadFloat(byte[] array, ref int offset)
         {
             byte[] a = new byte[4];
@@ -89,6 +99,7 @@ namespace Client
             return BitConverter.ToSingle(a, 0);
         }
 
+        // Size: 8
         public static double ReadDouble(byte[] array, ref int offset)
         {
             byte[] a = new byte[8];
